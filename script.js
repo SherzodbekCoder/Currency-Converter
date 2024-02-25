143d987d407447f4c1bff7068e033164264546f1
asosiymenu007 <asosiymenu007@gmail.com>
2024-02-25 18:44:26 +0500

    Currency Converter

---
+++ script.js
@@ -0,0 +1,52 @@
+const API_KEY = `b10ed48fe4d382157e9874d4`;
+
+let api = `https://v6.exchangerate-api.com/v6/${API_KEY}/latest/USD`;
+const fromDropDown = document.getElementById("from-currency-select");
+const toDropDown = document.getElementById("to-currency-select");
+
+currencies.forEach((currency) => {
+  const option = document.createElement("option");
+  option.value = currency;
+  option.text = currency;
+  fromDropDown.add(option);
+});
+
+
+currencies.forEach((currency) => {
+  const option = document.createElement("option");
+  option.value = currency;
+  option.text = currency;
+  toDropDown.add(option);
+});
+
+
+fromDropDown.value = "USD";
+toDropDown.value = "UZS";
+
+let convertCurrency = () => {
+  const amount = document.querySelector("#amount").value;
+  const fromCurrency = fromDropDown.value;
+  const toCurrency = toDropDown.value;
+
+
+  if (amount.length != 0) {
+    fetch(api)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        let fromExchangeRate = data.conversion_rates[fromCurrency];
+        let toExchangeRate = data.conversion_rates[toCurrency];
+        const convertedAmount = (amount / fromExchangeRate) * toExchangeRate;
+        result.innerHTML = `${amount} ${fromCurrency} = ${convertedAmount.toFixed(
+          2
+        )} ${toCurrency}`;
+      });
+  } else {
+    alert("Iltimos miqdoeni kiriting");
+  }
+};
+
+document
+  .querySelector("#convert-button")
+  .addEventListener("click", convertCurrency);
+window.addEventListener("load", convertCurrency);
